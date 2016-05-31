0a8bc40612ce07e87d4505282ea26dbc6d9d4f1f
Matthew Lake <mgtlake@outlook.com>
2016-06-01 00:31:57 +1000

    add helper functions

--- src/Juliet.jl
+++ src/Juliet.jl
@@ -2,4 +2,12 @@ module Juliet
 
 include("types.jl")
 
+function new_lesson(name; description="", version=v"1", authors=[],
+	keywords=[], questions=[])
+	return Types.Lesson(name, description, version, authors, keywords, questions)
+end
+
+function add_question!(lesson::Types.Lesson, question::Types.AbstractQuestion)
+	push!(lesson.questions, question)
+end
 end # module
