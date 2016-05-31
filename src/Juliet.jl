f4d2ea74b0f10f025322c6ea781cf498a56b2a9e
Matthew Lake <mgtlake@outlook.com>
2016-06-01 01:39:10 +1000

    package and save lessons

--- src/Juliet.jl
+++ src/Juliet.jl
@@ -1,5 +1,7 @@
 module Juliet
 
+using JLD
+
 include("types.jl")
 
 function new_lesson(name; description="", version=v"1", authors=[],
@@ -10,4 +12,15 @@ end
 function add_question!(lesson::Types.Lesson, question::Types.AbstractQuestion)
 	push!(lesson.questions, question)
 end
+
+function package_lesson(lesson::Types.Lesson, filename)
+	save(filename, "lesson: $(lesson.name)", lesson)
+end
+
+function package_course(course::Types.Course, filename)
+	save(filename, "course: $(lesson.name)", course)
+end
+
+end
+
 end # module
