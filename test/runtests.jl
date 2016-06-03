c8c8e81ee98290d755eb5ec5cfb5471bf1616b56
Matthew Lake <mgtlake@outlook.com>
2016-06-03 17:42:41 +1000

    add new test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,9 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+f(x) = strip(x) != ""
+lesson = Juliet.new_lesson("1234")
+Juliet.add_question!(lesson, Juliet.Types.FunctionQuestion("question 1",
+	["hint 1", "hint 2", "hint 3"], f))
+Juliet.complete_lesson(lesson)
