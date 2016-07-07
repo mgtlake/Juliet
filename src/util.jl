50659c7e840f17bb1d4fc653d217bde3117c32af
Matthew Lake <mgtlake@outlook.com>
2016-07-07 12:30:14 +1000

    Reimplement half of process.jl because _some_ programs don't give nice exit codes

--- src/util.jl
+++ src/util.jl
@@ -75,4 +75,36 @@ function package_course(course::Juliet.Types.Course, filename)
 	JLD.save(File(DataFormat{:JLD}, filename), course.name, course)
 end
 
+"""
+Reimplement half of process.jl because _some_ programs don't give nice
+exit codes
+"""
+function run(cmds::Base.AbstractCmd, args...; whitelist=[])
+    ps = Base.spawn(cmds, Base.spawn_opts_inherit(args...)...)
+    success(ps; whitelist=whitelist) ? nothing : Base.pipeline_error(ps)
+end
+
+function test_success(proc::Base.Process; whitelist=[])
+	assert(Base.process_exited(proc))
+	if proc.exitcode < 0
+		#TODO: this codepath is not currently tested
+		throw(Base.UVError("could not start process $(string(proc.cmd))", proc.exitcode))
+	end
+	(proc.exitcode == 0 || proc.exitcode in whitelist) &&
+		(proc.termsignal == 0 || proc.termsignal == Base.SIGPIPE
+		|| proc.termsignal in whitelist)
+end
+
+function success(x::Base.Process; whitelist=[])
+	Base.wait(x)
+	Base.kill(x)
+	test_success(x; whitelist=whitelist)
+end
+success(procs::Base.Vector{Base.Process}; whitelist=[]) =
+	mapreduce(success, &, procs; whitelist=whitelist)
+success(procs::Base.ProcessChain; whitelist=[]) =
+	success(procs.processes; whitelist=whitelist)
+success(cmd::Base.AbstractCmd; whitelist=[]) =
+	success(Base.spawn(cmd); whitelist=whitelist)
+
 end # module
