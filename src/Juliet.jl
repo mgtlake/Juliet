aa67f47f41c1b8ecbf8b310b3fbdbe88bc1910c3
Mike J Innes <mike.j.innes@gmail.com>
2016-08-09 11:00:21 +0100

    this didn't work with precompilation

--- src/Juliet.jl
+++ src/Juliet.jl
@@ -33,11 +33,11 @@ end
 """
 Get user input in a environment agnostic manner
 """
-macro getInput()
+function getInput()
 	if isdefined(Main, :Atom)
-		return :(Main.Atom.input())
+		return Main.Atom.input()
 	else
-		return :(readline())
+		return readline()
 	end
 end
 
@@ -82,7 +82,7 @@ function choose_lesson(courses::Vector{Types.Course})
 	print_options(courses, "Courses:")
 
 	input = AbstractString{}
-	while (print("> "); input = @getInput;
+	while (print("> "); input = getInput();
 			!isa(parse(input), Number) ||
 			!(0 < parse(Int, input) <= length(courses)))
 		@match strip(input) begin
@@ -105,7 +105,7 @@ function choose_lesson(course::Types.Course)
 		"Lessons in $(course.name) (type `!back` to return to the total list):")
 
 	input = AbstractString{}
-	while (print("> "); input = @getInput;
+	while (print("> "); input = getInput();
 			!isa(parse(input), Number) ||
 			!(0 < parse(Int, input) <= length(course.lessons)))
 		@match strip(input) begin
@@ -120,7 +120,7 @@ function choose_lesson(course::Types.Course)
 	complete_lesson(selection)
 	if selection != last(course.lessons)
 		println("Continue to next lesson in course? y/n")
-		while (input = strip(lowercase(@getInput));
+		while (input = strip(lowercase(getInput()));
 				!(input in ["yes", "y", "no", "n"]))
 			println("Invalid selection")
 		end
@@ -184,14 +184,14 @@ end
 
 function get_input(question)
 	print("> ")
-	input = @getInput
+	input = getInput()
 	# Remove ansii codes
 	return replace(input, r"\e\[([A-Z]|[0-9])", "")
 end
 
 function get_input(question::Types.InfoQuestion)
 	print("[Press Enter to continue]")
-	input = @getInput
+	input = getInput()
 	# Remove ansii codes
 	return replace(input, r"\e\[([A-Z]|[0-9])", "")
 end
