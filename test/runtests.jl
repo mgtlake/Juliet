fa1de187779265b94daaf4ef6de7bbdfbcead609
Matthew Lake <mgtlake@outlook.com>
2016-08-22 22:58:08 +1000

    Filter out files not to test
    Problems with .ds_store on macs

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,13 @@ include("unittest.jl")
 
 clean = x -> replace(x, "\r\n", "\n")
 
-for i in readdir("in")
+for i in filter(x -> isa(parse(x), Number), readdir("in"))
 	input = joinpath("in", i)
 	script = joinpath("script", "$i.jl")
 	output = joinpath("out", i)
 	try
 		run(pipeline(pipeline(`cat $input`, `julia $script`), stdout="temp"))
+	catch
 		run(pipeline(pipeline(`type $input`, `julia $script`), stdout="temp"))
 	end
 	open("temp", "r") do f
